ec6a35789ae12a88056d4c5254bb90cfdfa7b9eb
brandonidun <88982788+brandonidun@users.noreply.github.com>
2024-12-05 10:41:14 +0000

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <div class="bg-blue-500 text-white text-center p-4 rounded-md shadow-lg">
+    <h1 class="text-2xl font-bold">Hello, Tailwind!</h1>
+    <p class="mt-2 text-lg">If you see this styled, Tailwind is working!</p>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps<{
+  msg: string;
+}>();
+</script>
+
+<style scoped>
+</style>
